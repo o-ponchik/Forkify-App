e610f35a13096c82cbcafcd11657d31db8de73ec
Maria Matviian <matviyanm@gmail.com>
2022-12-19 15:48:31 -0500

    implemented_listening_for_load_and_hashchange_events

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,11 +29,15 @@ const renderSpiner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpiner(recipeContainer);
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
-      //   `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcac4`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -160,3 +164,5 @@ const showRecipe = async function () {
 };
 
 showRecipe();
+
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
