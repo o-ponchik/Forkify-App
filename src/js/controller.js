1861554ce36604cf5faf5ff6619d916e49f9515b
Maria Matviian <matviyanm@gmail.com>
2022-12-20 15:29:26 -0500

    implemented_error_and_success_messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,7 +25,7 @@ const controlRecipes = async function () {
     recipeView.render(model.state.recipe);
     // const recipeView = new recipeView(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    recipeView.renderError();
   }
 };
 
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,6 +5,8 @@ console.log(Fraction);
 class RecipeView {
   #parentElement = document.querySelector(".recipe");
   #data;
+  #errorMessage = "We could not find that recipe. Please try another one!";
+  #message = "";
 
   render(data) {
     this.#data = data;
@@ -25,6 +27,36 @@ class RecipeView {
           </svg>
   </div>`;
     this.#parentElement.innerHTML = "";
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+        <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>`;
+
+    this.#clear;
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderMesssage(message = this.#message) {
+    const markup = `
+        <div class="message">
+            <div>
+              <svg>
+                  <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>`;
+
+    this.#clear;
     this.#parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
@@ -94,8 +126,7 @@ class RecipeView {
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
           ${this.#data.ingredients.map(this.#generateMarkupIngredient).join("")}
-          
-
+        
           </ul>
         </div>
 
