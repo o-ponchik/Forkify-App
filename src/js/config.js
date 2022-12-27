4eae41b0d8cccbd5ee81e70394ea2989cd7975a8
Maria Matviian <matviyanm@gmail.com>
2022-12-27 10:40:21 -0500

    implementing_uploading_a_new_recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = "083119a4-3310-4460-b107-17511a00abac";
+export const MODAL_CLOSE_SEC = 2.5;
