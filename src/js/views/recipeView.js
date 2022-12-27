3633ff841a4497f01921b8b1dedab9e9966f3d2e
Maria Matviian <matviyanm@gmail.com>
2022-12-27 16:48:03 -0500

    setting_fracty_and_rebuild_app

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -133,7 +133,7 @@ class RecipeView extends View {
                 <use href="${icons}.svg#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ""
+                ing.quantity ? new fracty(ing.quantity).toString() : ""
               }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
