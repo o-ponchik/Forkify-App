471adf4957b784b05ff18bc3c0cd87239a6e15d3
Maria Matviian <matviyanm@gmail.com>
2022-12-27 16:39:00 -0500

    changed_Fraction_to_fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from "./View.js";
 
 import icons from "url:../../img/icons.svg";
-import { Fraction } from "fractional";
+import fracty from "fracty";
 
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
